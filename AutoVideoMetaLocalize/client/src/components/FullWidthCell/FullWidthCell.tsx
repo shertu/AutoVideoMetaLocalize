f0d7485c74f83317e09e4531dd1e089ddbfea57e
Jared Blackman <djared.xeknau@outlook.com>
2020-04-18 09:17:39 +1000

    Updated full width cell to allow for custom widths.

--- AutoVideoMetaLocalize/client/src/components/FullWidthCell/FullWidthCell.tsx
+++ AutoVideoMetaLocalize/client/src/components/FullWidthCell/FullWidthCell.tsx
@@ -6,23 +6,37 @@ import classNames from 'classnames';
 declare const Sizes: ["xs", "sm", "md", "lg", "xl"];
 
 export interface FullWidthUnitProps {
+  size?: typeof Sizes[number] | number,
   className?: string,
-  size?: typeof Sizes[number],
+}
+
+function isNumber(size?: typeof Sizes[number] | number): size is number {
+  return (size as number).toPrecision !== undefined;
 }
 
 /**
- * A page which center its elements and is full width up to a maximum
+ * A page which centers its elements and is full width up to a maximum.
  *
  * @param {object} props
  * @return {JSX.Element}
  */
 export const FullWidthCell: React.FunctionComponent<FullWidthUnitProps> = (props) => {
   const rowClassName = classNames('full-width-cell', props.className);
-  const colClassName = `full-width-${props.size || "md"}`;
+  let colStyle: React.CSSProperties;
+  let colClassName: string;
+
+  // assign default value
+  props.size = props.size || 'md';
+
+  if (isNumber(props.size)) {
+    colStyle = { maxWidth: props.size };
+  } else {
+    colClassName = `full-width-${props.size}`;
+  }
 
   return (
     <Row className={rowClassName} align="top" justify="center">
-      <Col className={colClassName}>
+      <Col className={colClassName} style={colStyle}>
         {props.children}
       </Col>
     </Row>
