6f6fc6cdf724f431cdaefcc44310776b142b313c
Jared Blackman <djared.xeknau@outlook.com>
2020-06-15 04:02:47 +1000

    Google Auth Compliancy Update

--- AutoVideoMetaLocalize/client/src/components/App/AppLayout/AppLayout.tsx
+++ AutoVideoMetaLocalize/client/src/components/App/AppLayout/AppLayout.tsx
@@ -1,8 +1,10 @@
-import {Layout, Row} from 'antd';
+import { Layout, Row, Button } from 'antd';
 import * as React from 'react';
-import {AppContentSwitch} from '../AppContentSwitch/AppContentSwitch';
+import { AppContentSwitch } from '../AppContentSwitch/AppContentSwitch';
+import { Link } from 'react-router-dom';
+import routes from '../../../routes';
 
-const {Content} = Layout;
+const { Content, Footer } = Layout;
 
 /**
  * The general layout or structure of the web application, e.g. main, header, footer, etc.
@@ -11,12 +13,17 @@ const {Content} = Layout;
  */
 export function AppLayout(): JSX.Element {
   return (
-    <Layout style={{minHeight: '100vh'}}>
-      <Content style={{padding: '40px 8px'}}>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Content style={{ padding: '40px 8px' }}>
         <Row id="content-alignment" justify="center">
           <AppContentSwitch />
         </Row>
       </Content>
+      <Footer>
+        <Link to={routes.ROUTE_PRIVACY_POLICY}>
+          <Button type="link">Privacy Policy</Button>
+        </Link>
+      </Footer>
     </Layout>
   );
 }
