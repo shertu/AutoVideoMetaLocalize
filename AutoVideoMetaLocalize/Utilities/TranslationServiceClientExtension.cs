9cf38e73e09e42932fd150199058f63fcf6d370a
Jared Blackman <djared.xeknau@outlook.com>
2020-08-01 23:46:15 +1000

    Update from home.

--- AutoVideoMetaLocalize/Utilities/TranslationServiceClientExtension.cs
+++ AutoVideoMetaLocalize/Utilities/TranslationServiceClientExtension.cs
@@ -7,21 +7,17 @@ using System.Web;
 
 namespace AutoVideoMetaLocalize.Utilities {
 	public static class TranslationServiceClientExtension {
-		public const string HTML_BR = @"<br />";
-
 		public static async Task<string> SimpleTranslation(this TranslationServiceClient service, string parent, string targetLanguageCode, string sourceLanguageCode, string text) {
 			if (targetLanguageCode == sourceLanguageCode) {
 				return text;
 			}
 
-			// replace line breaks
-			string htmlEncodedText = Regex.Replace(text, @"\r\n?|\n", HTML_BR);
-
 			TranslateTextRequest request = new TranslateTextRequest {
 				Parent = parent,
 				TargetLanguageCode = targetLanguageCode,
 				SourceLanguageCode = sourceLanguageCode,
-				Contents = { htmlEncodedText },
+				Contents = { text },
+				MimeType = "text/plain",
 			};
 
 			TranslateTextResponse response = await service.TranslateTextAsync(request);
@@ -30,9 +26,7 @@ namespace AutoVideoMetaLocalize.Utilities {
 			// Translations are HTML encoded
 			using (StringWriter sw = new StringWriter()) {
 				HttpUtility.HtmlDecode(translation, sw);
-				string temp = sw.ToString().Replace($" {HTML_BR} ", "\n");
-				//temp = Regex.Replace(temp, @"<|>", string.Empty);
-				return temp;
+				return sw.ToString();
 			}
 		}
 	}
