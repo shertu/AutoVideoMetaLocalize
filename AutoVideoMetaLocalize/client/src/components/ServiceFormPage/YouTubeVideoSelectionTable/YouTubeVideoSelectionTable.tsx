482526c313b12b42b9d7845ab86289e71fd4fb50
Jared Blackman <djared.xeknau@outlook.com>
2020-09-09 22:31:52 +1000

    Update from home.

--- AutoVideoMetaLocalize/client/src/components/ServiceFormPage/YouTubeVideoSelectionTable/YouTubeVideoSelectionTable.tsx
+++ AutoVideoMetaLocalize/client/src/components/ServiceFormPage/YouTubeVideoSelectionTable/YouTubeVideoSelectionTable.tsx
@@ -165,7 +165,7 @@ export function YouTubeVideoSelectionTable(props: YouTubeVideoSelectionTableProp
         <Skeleton loading={loading} active>
           <FormSelectionTable
             table={{
-              className: "max-cell-sm",
+              rowClassName: "max-cell-sm",
               dataSource: channelUploadsPlaylistItems,
               pagination: pagination,
               rowKey: rowKey,
